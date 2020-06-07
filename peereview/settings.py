bb5f4f17558952003af6569ebc71f26c1f16af67
rmautia <raphaelnyangenya@gmail.com>
2020-06-08 00:24:33 +0300

    add installed applications

--- peereview/settings.py
+++ peereview/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'rest_framework',
     'bootstrap4',
-    'ratings',
+    'ratings.apps.RatingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ratings/apps.py
+++ ratings/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class RateConfig(AppConfig):
+class RatingsConfig(AppConfig):
     name = 'rate'
