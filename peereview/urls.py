e79e987204a393123a5f5b1bf115827871667e33
rmautia <raphaelnyangenya@gmail.com>
2020-06-07 20:12:10 +0300

    licence information for the application

--- peereview/urls.py
+++ peereview/urls.py
@@ -21,4 +21,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('ratings.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^api-token-auth/', obtain_auth_token)
 ]
